b377490416ff6247590cc65e4835e507b348a3d1
Anthony <anthony.damato@hotmail.fr>
2019-03-05 17:57:14 +0100

    build in local and smaller images

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ echo "**** Building base image ****"
 docker build -t base/rust base 
 
 echo "**** Building service 1 from base ****"
-docker build -t service_1 services/service_1
+docker build -t service_1:latest services/service_1
 
 echo "**** Building service 2 from base ****"
-docker build -t service_2 services/service_2
+docker build -t service_2:latest services/service_2
